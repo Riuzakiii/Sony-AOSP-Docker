9ca28715525547593e48f0be68052ed5ef66bb6d
Riuzakiii <66674723+Riuzakiii@users.noreply.github.com>
2023-03-09 12:17:47 +0100

    Fix for building scripts, dont use less than 1 core x)

--- 7.1.2/build_kernel.sh
+++ 7.1.2/build_kernel.sh
@@ -2,6 +2,14 @@
 
 export CROSS_COMPILE="/home/android/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
 
+if [ $(($(nproc) - 3)) -gt 0 ]
+then
+    USE_N_CPUS=$(($(nproc) - 3))
+else
+    USE_N_CPUS=1
+fi
+echo "Using $USE_N_CPUS threads"
+
 if [ ! -d "kernel" ]
 then
     git clone https://github.com/sonyxperiadev/kernel
@@ -16,7 +24,7 @@ else
 fi
 
 make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE aosp_tone_kagura_defconfig
-make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE -j "$(($(nproc) - 3))"
+make ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE -j "$USE_N_CPUS"
 
 echo "... Copying into device/sony/common-kernel/kernel-"
 
--- 8.1.0/run_build.sh
+++ 8.1.0/run_build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+if [ $(($(nproc) - 3)) -gt 0 ]
+then
+    USE_N_CPUS=$(($(nproc) - 3))
+else
+    USE_N_CPUS=1
+fi
+echo "Using $USE_N_CPUS threads"
+
 PATH=~/bin:$PATH
 USER=$(whoami)
 export PATH
 export USER
 source build/envsetup.sh
-lunch && make -j "$(($(nproc) - 3))"
+lunch && make -j "$USE_N_CPUS"
